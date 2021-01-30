4835f7a7cde8e5d7cbff87eca244026df8b2531b
axemanofic <axeman.ofic@gmail.com>
2021-01-30 14:49:15 +0300

    small fix

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
+import os
 import handlers
 import callback
 from misc import bot
 from telebot import types
 from flask import Flask, request
 from flask_sslify import SSLify
-from settings.config import TOKEN
+from settings.config import TOKEN, WEBHOOK_HOST
 
 app = Flask(__name__)
 sslify = SSLify(app)
 
 
-@app.route('/()'.format(TOKEN), methods=['post'])
+@app.route('/{}'.format(TOKEN), methods=['post'])
 def index():
     bot.process_new_updates([types.Update.de_json(request.stream.read().decode('utf-8'))])
     return "ok", 200
 
+@app.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=WEBHOOK_HOST + TOKEN)
+    return "ok", 200
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
--- settings/config.py
+++ settings/config.py
@@ -1,3 +1,3 @@
 TOKEN = '1601895519:AAElWVSag74WVPHcUOQmbvuyWgw06FiPnZ0'
-WEBHOOK_HOST = 'https://eagletailbot.herokuapp.com//{}'
+WEBHOOK_HOST = 'https://eagletailbot.herokuapp.com/{}'
 ID = 494061970
